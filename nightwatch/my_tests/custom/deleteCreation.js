ca7d156cc264e889838f8a94e066570d642897eb
michael-maf <michael@matterandform.net>
2015-05-15 15:57:45 -0700

    add assert.title('profile') to login

--- nightwatch/my_tests/custom/deleteCreation.js
+++ nightwatch/my_tests/custom/deleteCreation.js
@@ -1,9 +1,10 @@
 var sel = require('./../assets/selector_module.js');
 
+var info = sel.creations.creations_creation.info;
+var edit = sel.creations.creations_creation.action.edit;
+
 exports.command = function(callback) {
 	var self = this;
-
-	this.
 
 
 
--- nightwatch/my_tests/custom/qualityChange.js
+++ nightwatch/my_tests/custom/qualityChange.js
@@ -1,14 +1,9 @@
 var sel = require ('./../assets/selector_module.js');
 
-exports.command = function (gridIndex, qualIndex, callback) {
+exports.command = function (qualIndex, callback) {
     var self = this;
     this
-        //.frame(null)
-		.pause(1000)
-        .waitForElementVisible(sel.creations.article.getNth_article(gridIndex), 1000, function() {	
-			this.click(sel.creations.article.getNth_article(gridIndex));
-		})
-        .waitForElementVisible(sel.creations.creations_creation.preview.quality.quality_button, 3000, function() {
+    	.waitForElementVisible(sel.creations.creations_creation.preview.quality.quality_button, 3000, function() {
 			this.click(sel.creations.creations_creation.preview.quality.quality_button);
 		})
 		.waitForElementVisible(sel.creations.creations_creation.preview.quality.getNth_button(qualIndex), 1000, function() {
@@ -16,8 +11,8 @@ exports.command = function (gridIndex, qualIndex, callback) {
 		})
 		.pause(1000);
 
-    if( typeof callback === "function"){
+    if(typeof callback === "function")
         callback.call(self);
-    }
+    
     return this; // allows the command to be chained.
 };

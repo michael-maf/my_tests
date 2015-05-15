508895de1e71229a3c22eade265a884860d2f4c0
michael-maf <michael@matterandform.net>
2015-05-15 15:40:39 -0700

    update qualityChange

--- nightwatch/my_tests/custom/qualityChange.js
+++ nightwatch/my_tests/custom/qualityChange.js
@@ -1,20 +1,18 @@
 var sel = require ('./../assets/selector_module.js');
 
-exports.command = function (
-//gridIndex, qualIndex, 
-callback) {
+exports.command = function (gridIndex, qualIndex, callback) {
     var self = this;
     this
         //.frame(null)
 		.pause(1000)
-        .waitForElementVisible(sel.creations.article.getNth_article(1), 1000, function() {	
-			this.click(sel.creations.article.getNth_article(1));
+        .waitForElementVisible(sel.creations.article.getNth_article(gridIndex), 1000, function() {	
+			this.click(sel.creations.article.getNth_article(gridIndex));
 		})
         .waitForElementVisible(sel.creations.creations_creation.preview.quality.quality_button, 3000, function() {
 			this.click(sel.creations.creations_creation.preview.quality.quality_button);
 		})
-		.waitForElementVisible(sel.creations.creations_creation.preview.quality.getNth_button(1), 1000, function() {
-			this.click(sel.creations.creations_creation.preview.quality.getNth_button(1));
+		.waitForElementVisible(sel.creations.creations_creation.preview.quality.getNth_button(qualIndex), 1000, function() {
+			this.click(sel.creations.creations_creation.preview.quality.getNth_button(qualIndex));
 		})
 		.pause(1000);
 

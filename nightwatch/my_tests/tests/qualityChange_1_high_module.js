730d4404b1cdb872b51e4159af3bb91a99d6e7b8
muhammad-maf <muhammad@matterandform.net>
2015-05-15 15:29:01 -0400

    Add quality change tests

---
+++ nightwatch/my_tests/tests/qualityChange_1_high_module.js
@@ -0,0 +1,12 @@
+var sel = require('./selector_module.js');
+
+module.exports = {
+  "Select from creations grid: 1; Change quality: high" : function (client) {
+	client
+		.url('http://alpha-preview.cashew3d.com/creations')
+		.waitForElementVisible('body', 1000, function() {
+			client.qualityChange(1,3);
+		})
+		.end();
+  }
+}
---
+++ nightwatch/my_tests/tests/qualityChange_1_low_module.js
@@ -0,0 +1,12 @@
+var sel = require('./selector_module.js');
+
+module.exports = {
+  "Select from creations grid: 1; Change quality: low" : function (client) {
+	client
+		.url('http://alpha-preview.cashew3d.com/creations')
+		.waitForElementVisible('body', 1000, function() {
+			client.qualityChange(1,1);
+		})
+		.end();
+  }
+}

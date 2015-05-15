2c0b02448e17937b0216fcc9b005125837df0c69
muhammad-maf <muhammad@matterandform.net>
2015-05-15 17:48:07 -0400

    Added download.js function and download_module.js test

--- nightwatch/my_tests/custom/download.js
+++ nightwatch/my_tests/custom/download.js
@@ -3,13 +3,15 @@ var sel = require ('./../assets/selector_module.js');
 exports.command = function (callback) {
     var self = this;
     this
-    	.waitForElementVisible(sel.creations.creations_creation.action.save_button, 3000, function() {
+		.waitForElementVisible(sel.creations.creations_creation.action.save_button, 1000, function () {
 			this.click(sel.creations.creations_creation.action.save_button);
 		})
+		.waitForElementVisible(sel.creations.creations_creation.action.save_prompt.download_button, 1000, function () {
+			this.click(sel.creations.creations_creation.action.save_prompt.download_button);
+		})
 		.pause(1000)
-		.acceptAlert()
+		.acceptAlert();
     if (typeof callback === "function")
         callback.call(self);
-    
     return this; // allows the command to be chained.
 };
--- nightwatch/my_tests/tests/download_module.js
+++ nightwatch/my_tests/tests/download_module.js
@@ -1,15 +1,15 @@
 var sel = require ('./../assets/selector_module.js');
 
-exports.command = function (callback) {
-    var self = this;
-    this
-    	.waitForElementVisible(sel.creations.creations_creation.action.save_button, 3000, function() {
-			this.click(sel.creations.creations_creation.action.save_button);
-		})
-		.pause(1000)
-		.acceptAlert()
-    if (typeof callback === "function")
-        callback.call(self);
-    
-    return this; // allows the command to be chained.
-};
+module.exports = {
+	'Download a model' : function (browser) {
+		browser
+			.loginJohnny()
+			.pause(1000)
+			.waitForElementVisible(sel.profile.article.getNth_article(1), 3000, function() {
+				browser.click(sel.creations.article.getNth_article(1));
+			})
+			.pause(1000)
+			.download()
+			.pause();
+	}
+}

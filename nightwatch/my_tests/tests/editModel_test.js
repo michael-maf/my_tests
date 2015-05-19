4804ef96f4111ab620ea49ea340fd82627d21fa7
michael-maf <michael@matterandform.net>
2015-05-19 11:41:40 -0700

    fix bug with getInfo's privacy

--- nightwatch/my_tests/tests/editModel_test.js
+++ nightwatch/my_tests/tests/editModel_test.js
@@ -1,15 +1,22 @@
 var sel = require('./../assets/selector_module');
 var info = sel.creations.creations_creation.info;
 
+var details = ["Potato", "Tomato", ["wasd", "potato", "ham"], false, 2];
+
 module.exports = {
 	'Edit Model' : function(browser) {
 		browser
 			.loginJohnny()
 			.clickElement(sel.profile.article.getNth_article(1))
 			.clickElement(info.edit.edit_button)
-			.fillForm("Potato", "Tomato", ["wasd", "potato", "ham"], false, 2)
+			.fillForm(details[0], details[1], details[2], details[3], details[4])
 			.clickElement(info.edit.save_button)
-			.pause(10000)
+			.getInfo(function(title, desc, tags, public, license) {
+				console.log(title, desc, tags, public, license);
+				for(var i = 0; i < arguments.length; i++)
+					browser.assert.deepEqual(arguments[i], details[i]);
+			})
+			.pause(1000)
 			.end();
 	}
 };

2db1cf047e58423d6499163fdc22139fa35e9a33
muhammad-maf <muhammad@matterandform.net>
2015-05-19 11:06:23 -0400

    Add login_test.js, revert changes to login_module.js

--- nightwatch/my_tests/tests/login_module.js
+++ nightwatch/my_tests/tests/login_module.js
@@ -9,11 +9,6 @@ module.exports = {
 	'Login': function(browser) {
 		browser
 			.loginJohnny()
-			.assert.visible(sel.profile.avatar_image)
-			.assert.visible(sel.profile.fullname_text)
-			.assert.visible(sel.profile.creationsCount_text)
-			.assert.visible(sel.profile.creatingSince_text)
-			.assert.visible(sel.profile.logout_button)
 			.end();
 	}
 };
---
+++ nightwatch/my_tests/tests/login_test.js
@@ -0,0 +1,19 @@
+/*
+	Muhammad Mousa 5/19/2015
+	Test for the login page on the alpha Cashew website.
+*/
+
+var sel = require('./../assets/selector_module.js');
+
+module.exports = {
+	'Login Test': function(browser) {
+		browser
+			.loginJohnny()
+			.assert.visible(sel.profile.avatar_image)
+			.assert.visible(sel.profile.fullname_text)
+			.assert.visible(sel.profile.creationsCount_text)
+			.assert.visible(sel.profile.creatingSince_text)
+			.assert.visible(sel.profile.logout_button)
+			.end();
+	}
+};

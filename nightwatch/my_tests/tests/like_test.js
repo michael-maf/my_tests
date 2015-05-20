89392798c08e1958ab50ea9b3d802070c1f94506
michael-maf <michael@matterandform.net>
2015-05-20 14:18:09 -0700

    create like_test

--- nightwatch/my_tests/tests/like_test.js
+++ nightwatch/my_tests/tests/like_test.js
@@ -3,6 +3,8 @@ var action = sel.creations.creations_creation.action;
 
 module.exports = {
 	'Like a creation': function(browser) {
+		//assumes that you are liking a model with 0 likes.
+		var likecount = 0;
 		browser
 			.url("http://alpha-preview.cashew3d.com/creations")
 			.clickElement(sel.creations.article.getNth_article(1))
@@ -12,7 +14,16 @@ module.exports = {
 			.assert.title("Profile")
 			.clickElement(sel.profile.article.getNth_article(1))
 			.assert.visible(action.nice_button)
+			.assert.elementPresent(action.nice_prompt.niceCount_button)
+			.getText(action.nice_prompt.niceCount_button, function(result) {
+				if(result.value)
+					likecount = parseInt(result.value);
+			})
 			.clickElement(action.nice_button)
+			.getText(action.nice_prompt.niceCount_button, function(result) {
+				browser.assert.deepEqual(++result.value, likecount);
+			})
+			.pause(10000)
 			.end();
 	}
 };
